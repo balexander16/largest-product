9ac2dd86e8b33b89b60a5a9b9a99b7d7ea50f41b
Brian <babta29@gmail.com>
2019-05-30 09:33:43 -0600

    Solution.

--- src/edu/cnm/deepdive/Problem11.java
+++ src/edu/cnm/deepdive/Problem11.java
@@ -10,11 +10,9 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-//  Will open a file and return a two dimensional array of ints.
 public class Problem11 {
 
   private static int[][] readFile(String filename) throws IOException {
-
     try (
         InputStream input = new FileInputStream(filename);
         Reader reader = new InputStreamReader(input);
@@ -32,12 +30,61 @@ public class Problem11 {
       }
       return workList.toArray(new int[workList.size()][]);
     }
+  }
 
+  private static long getMaxProduct(int[][] data, int productLength) {
+    long maxProduct = 0;
+    for (int row = 0; row < data.length; row++) {
+      for (int col = 0; col < data[row].length; col++) {
+        for (Direction direction : Direction.values()) {
+          try {
+            int rowOffset = direction.getRowOffset();
+            int colOffset = direction.getColOffset();
+            long product = 1;
+            for (int step = 0; step < productLength; step++) {
+              product *= data[row + step * rowOffset][col + step * colOffset];
+            }
+            if (product > maxProduct) {
+              maxProduct = product;
+            }
+          } catch (ArrayIndexOutOfBoundsException e) {
+            // DO NOTHING - GET ON WITH LIFE!
+          }
+        }
+      }
+    }
+    return maxProduct;
   }
 
   public static void main(String[] args) throws IOException {
-  int[][] data = readFile("largest-product-data.txt");
-  System.out.println(Arrays.toString(data));
+    int[][] data = readFile("largest-product-data.txt");
+    long maxProduct = getMaxProduct(data, 4);
+    System.out.printf("%,d%n", maxProduct);
+  }
+
+  private enum Direction {
+
+    NORTH_EAST(-1, 1),
+    EAST(0, 1),
+    SOUTH_EAST(1, 1),
+    SOUTH(1, 0);
+
+    private final int rowOffset;
+    private final int colOffset;
+
+    Direction(int rowOffset, int colOffset) {
+      this.rowOffset = rowOffset;
+      this.colOffset = colOffset;
+    }
+
+    public int getRowOffset() {
+      return rowOffset;
+    }
+
+    public int getColOffset() {
+      return colOffset;
+    }
+
   }
 
 }
